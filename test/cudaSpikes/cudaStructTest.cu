9d9888a3e65637b1b3d6bc44de9f7e7f4a9166c6
timaaagh <timaaagh@comcast.net>
2014-10-22 19:41:21 -0400

    Made example of how to use cuda

--- test/cudaSpikes/cudaStructTest.cu
+++ test/cudaSpikes/cudaStructTest.cu
@@ -7,7 +7,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "book.h"
+
 #define N 10
+#define BLOCKS 1
+#define THREADS 10
 
 typedef struct{
     int id;
@@ -15,19 +19,18 @@ typedef struct{
     int height;
 } Person;
 
-Person * populate_array();
 
-__global__ void analyze_id()
+__global__ void analyze_id(Person *people, int *statResults)
 {
 
 }
 
-__global__ void analyze_age()
+__global__ void analyze_age(Person *people, int *statResults)
 {
     
 }
 
-__global__ void analyze_height()
+__global__ void analyze_height(Person *people, int *statResults)
 {
     
 }
@@ -35,41 +38,40 @@ __global__ void analyze_height()
 
 int main(void)
 {
-    Person * people;
-    people = populate_array();
+    // CPU variables
+    Person *people;
+    int *idStats[N];
+    int *ageStats[N];
+    int *heightStats[N];
 
-    int x;
-    for(x = 0; x < 1; x++)
-    {
-        printf("id = %d\n", people[x].id);
-        printf("age = %d\n", people[x].age);
-        printf("height = %d\n", people[x].height);
-        printf("\n");
-    }
+    // GPU variables
+    Person *dev_people;
+    int *dev_idStats;
+    int *dev_ageStats;
+    int *dev_heightStats;
 
-    return 0;
+    // creates zero-copy memory for buffer (both CPU and GPU point to same memory). A pointer will be given to the GPU later...
+    HANDLE_ERROR( cudaHostAlloc( (void **) &people, N * sizeof(Person), cudaHostAllocWriteCombined | cudaHostAllocMapped));
 
+    // allocating GPU memory (GPU only memory)
+    HANDLE_ERROR(cudaMalloc( (void **) &dev_idStats, N * sizeof(int) ));
+    HANDLE_ERROR(cudaMalloc( (void **) &dev_ageStats, N * sizeof(int) ));
+    HANDLE_ERROR(cudaMalloc( (void **) &dev_heightStats, N * sizeof(int) ));
 
-}
-
-Person * populate_array()
-{
-    Person people[10];
-    //people = malloc(N * sizeof(Person));
-
-    Person person1 = {1, 25, 6};
-   /* Person *person2 = {2, 25, 6};
-    Person *person3 = {3, 25, 6};
-    Person *person4 = {4, 25, 6};
-    Person *person5 = {5, 25, 6};
-    Person *person6 = {6, 25, 6};
-    Person *person7 = {7, 25, 6};
-    Person *person8 = {8, 25, 6};
-    Person *person9 = {9, 25, 6};
-    Person *person10 = {10, 25, 6};*/
+    // fill people with some...
+    Person person1  = {1, 25, 6};
+    Person person2  = {2, 25, 5};
+    Person person3  = {3, 25, 6};
+    Person person4  = {4, 26, 5};
+    Person person5  = {5, 26, 6};
+    Person person6  = {6, 26, 5};
+    Person person7  = {7, 27, 6};
+    Person person8  = {8, 27, 5};
+    Person person9  = {9, 27, 6};
+    Person person10 = {10, 28, 5};
 
     people[0] = person1;
-   /* people[1] = person2;
+    people[1] = person2;
     people[2] = person3;
     people[3] = person4;
     people[4] = person5;
@@ -77,8 +79,35 @@ Person * populate_array()
     people[6] = person7;
     people[7] = person8;
     people[8] = person9;
-    people[9] = person10;*/
+    people[9] = person10;
 
-    return people;
+    // check data in people
+    int x;
+    for(x = 0; x < N; x++)
+    {
+        printf("id = %d\n", people[x].id);
+        printf("age = %d\n", people[x].age);
+        printf("height = %d\n", people[x].height);
+        printf("\n");
+    }
+
+    // gives a pointer to the GPU to reference the zero-copy memory
+    HANDLE_ERROR(cudaHostGetDevicePointer(&dev_people, people, 0));
+
+    // calls to cuda kernels
+    analyze_id<<<BLOCKS, THREADS>>>(dev_people, dev_idStats);
+    analyze_age<<<BLOCKS, THREADS>>>(dev_people, dev_ageStats);
+    analyze_height<<<BLOCKS, THREADS>>>(dev_people, dev_heightStats);
+
+    // Get the results from the GPU
+    HANDLE_ERROR(cudaMemcpy(idStats, &dev_idStats, N * sizeof(int), cudaMemcpyDeviceToHost));
+    HANDLE_ERROR(cudaMemcpy(ageStats, &dev_ageStats, N * sizeof(int), cudaMemcpyDeviceToHost));
+    HANDLE_ERROR(cudaMemcpy(heightStats, &dev_heightStats, N * sizeof(int), cudaMemcpyDeviceToHost));
+
+    HANDLE_ERROR(cudaThreadSynchronize());
+
+    return 0;
+
 
 }
+
