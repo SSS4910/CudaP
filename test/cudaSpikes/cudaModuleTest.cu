3f2f6601370484021e6a2f8afd54271fe69cb371
timaaagh <timaaagh@comcast.net>
2014-10-28 21:25:38 -0400

    Turned cuda tests into usable modules

---
+++ test/cudaSpikes/cudaModuleTest.cu
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//#include "book.h"
+#include "cuda.cuh"
+
+
+
+int main(void)
+{
+    Buffer buffer;
+    analyze_data(&buffer);
+}
+
+
+
