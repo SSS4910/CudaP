8131f8d88c219f929d5a047cdcff6a8b1a976ce5
timaaagh <timaaagh@comcast.net>
2014-11-21 18:48:02 -0500

    you know...the graphs

--- www/dashboard/index.php
+++ www/dashboard/index.php
@@ -54,8 +54,8 @@ include_once("./../resources/templates/navbar.php");
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 
-  <script src="js/highcharts.js"></script>
-  <script src="js/modules/exporting.js"></script>
+  <!-- <script src="js/highcharts.js"></script>
+  <script src="js/modules/exporting.js"></script> -->
 
 
 
