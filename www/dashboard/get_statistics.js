6a6af809f82e809861f8565692fb572f08eb815f
timaaagh <timaaagh@comcast.net>
2014-11-22 11:09:00 -0500

    Added monthly stats graph

--- www/dashboard/get_statistics.js
+++ www/dashboard/get_statistics.js
@@ -32,8 +32,15 @@ function get_statistics()
 			alert(monthlyStats);
 			alert(list404);
 			alert(listInject);*/
+
+			var intGeneralStats = new Array();
+			for(var x = 0; x < 4; x++)
+			{
+				intGeneralStats[x] = parseInt(generalStats[x]);
+			}
+
 			var intHourlyStats = new Array();
-			for(var x = 0; x < 24; x++)
+			for(x = 0; x < 24; x++)
 			{
 				intHourlyStats[x] = parseInt(hourlyStats[x]);
 			}
@@ -43,6 +50,64 @@ function get_statistics()
 			{
 				intMonthlyStats[x] = parseInt(monthlyStats[x]);
 			}
+
+			$(function () {
+				$('#general_stats').highcharts({
+				    chart: {
+				        type: 'bar'
+				    },
+				    title: {
+				        text: 'Basic statistics'
+				    },
+				    subtitle: {
+				        text: ''
+				    },
+				    xAxis: {
+				        categories: ['200s', '404s', 'Injections', 'total visits'],
+				        title: {
+				            text: null
+				        }
+				    },
+				    yAxis: {
+				        min: 0,
+				        title: {
+				            text: 'Population (millions)',
+				            align: 'high'
+				        },
+				        labels: {
+				            overflow: 'justify'
+				        }
+				    },
+				    tooltip: {
+				        valueSuffix: ' millions'
+				    },
+				    plotOptions: {
+				        bar: {
+				            dataLabels: {
+				                enabled: true
+				            }
+				        }
+				    },
+				    legend: {
+				        layout: 'vertical',
+				        align: 'right',
+				        verticalAlign: 'top',
+				        x: -40,
+				        y: 100,
+				        floating: true,
+				        borderWidth: 1,
+				        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+				        shadow: true
+				    },
+				    credits: {
+				        enabled: false
+				    },
+				    series: [{
+				        name: 'Occurrences',
+				        data: [intGeneralStats[0], intGeneralStats[1], intGeneralStats[2], intGeneralStats[3]]
+				    }]
+				});
+			});
 
 			$(function () {
 				$('#hourly_stats').highcharts({
@@ -85,47 +150,48 @@ function get_statistics()
 				        		intHourlyStats[16], intHourlyStats[17], intHourlyStats[18], intHourlyStats[19], intHourlyStats[20], intHourlyStats[21], intHourlyStats[22], intHourlyStats[23]]
 				    }]
 				});
-				});
+			});
 
 				$(function () {
-				$('#monthly_stats').highcharts({
-				    title: {
-				        text: 'Months of Access',
-				        x: -20 //center
-				    },
-				    subtitle: {
-				        text: 'Number of Visits per Month',
-				        x: -20
-				    },
-				    xAxis: {
-				        categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
-				        			 'August', 'September', 'October', 'November','December']
-				    },
-				    yAxis: {
-				        title: {
-				            text: 'Number of Visits'
-				        },
-				        plotLines: [{
-				            value: 0,
-				            width: 1,
-				            color: '#808080'
-				        }]
-				    },
-				    tooltip: {
-				        valueSuffix: 'Visits'
-				    },
-				    legend: {
-				        layout: 'vertical',
-				        align: 'right',
-				        verticalAlign: 'middle',
-				        borderWidth: 0
-				    },
-				    series: [{
-				        name: 'Visits',
-				        data: [intMonthlyStats[0], intMonthlyStats[1], intMonthlyStats[2], intMonthlyStats[3], intMonthlyStats[4], intMonthlyStats[5], intMonthlyStats[6], intMonthlyStats[7], 
-				        		intMonthlyStats[8], intMonthlyStats[9], intMonthlyStats[10], intMonthlyStats[11]]
-				    }]
-				});
+					$('#monthly_stats').highcharts({
+					    title: {
+					        text: 'Months of Access',
+					        x: -20 //center
+					    },
+					    subtitle: {
+					        text: 'Number of Visits per Month',
+					        x: -20
+					    },
+					    xAxis: {
+					        categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+					        			 'August', 'September', 'October', 'November','December']
+					    },
+					    yAxis: {
+					        title: {
+					            text: 'Number of Visits'
+					        },
+					        plotLines: [{
+					            value: 0,
+					            width: 1,
+					            color: '#808080'
+					        }]
+					    },
+					    tooltip: {
+					        valueSuffix: 'Visits'
+					    },
+					    legend: {
+					        layout: 'vertical',
+					        align: 'right',
+					        verticalAlign: 'middle',
+					        borderWidth: 0
+					    },
+					    series: [{
+					        name: 'Visits',
+					        data: [intMonthlyStats[0], intMonthlyStats[1], intMonthlyStats[2], intMonthlyStats[3], intMonthlyStats[4], 
+					        		intMonthlyStats[5], intMonthlyStats[6], intMonthlyStats[7], intMonthlyStats[8], intMonthlyStats[9], 
+					        		intMonthlyStats[10], intMonthlyStats[11]]
+					    }]
+					});
 				});
 			
 			
