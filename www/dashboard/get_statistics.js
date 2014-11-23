1636adaeac90ae77be19813097ea4960c5b3403c
timaaagh <timaaagh@comcast.net>
2014-11-23 17:44:32 -0500

    Changing units on top requests graph

--- www/dashboard/get_statistics.js
+++ www/dashboard/get_statistics.js
@@ -68,7 +68,7 @@ function graph_generalStats(generalStats)
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y: %d}</b></td></tr>',
+                '<td style="padding:0"><b>{point.y: .1f}</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
@@ -229,7 +229,7 @@ function graph_topRequests(listTopReq)
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y: %d}</b></td></tr>',
+                '<td style="padding:0"><b>{point.y: .1f}</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true,
